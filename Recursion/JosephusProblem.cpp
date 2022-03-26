5aca3ffeeb8ff29541a37d03468f519fde7a1ac3
atharvarana <89778955+atharvarana@users.noreply.github.com>
2022-03-27 00:04:10 +0530

    Add files via upload

---
+++ Recursion/JosephusProblem.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int jos(int n,int k){
+    if(n==1){
+        return 0;
+    }
+    else{
+        return (jos(n-1,k)+k)%n;
+    }
+}
+
+int main(){
+    int n,k;
+    cout<<"enter n and k :  ";
+    cin>>n>>k;
+    cout<<jos(n,k);
+    return 0;
+}
---
+++ Recursion/subsets.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void subsets(string s,string curr="" ,int i=0){
+    if(i==s.length()){
+        cout<<curr<<" ";
+        return;
+    }
+    subsets(s,curr,i+1);
+    subsets(s,curr+s[i],i+1);
+}
+int main(){
+    string s;
+    cout<<"enter string s : ";
+    cin>>s;
+    int i;
+    string curr;
+    subsets(s,"",0);
+    return 0;
+}
